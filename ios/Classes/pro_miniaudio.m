58a7849552cc1723234ad7c09f7aa2c14e134dd4
Andrey Ivanov <this.andrey@gmail.com>
2024-12-01 15:09:56 +0200

    fix ios/macos build

--- ios/Classes/pro_miniaudio.m
+++ ios/Classes/pro_miniaudio.m
@@ -1,5 +1,6 @@
 #include "../../native/src/audio_context.c"
 #include "../../native/src/logger.c"
+#include "../../native/src/waveform.c"
 #include "../../native/src/miniaudio.c"
 #include "../../native/src/playback_device.c"
 #include "../../native/src/resource_manager.c"
